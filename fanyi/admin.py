3730d8c98b428b5a2c85bfaf650a18ae9c516d1a
zjjfx88 <zjj7842806@163.com>
2018-04-09 13:13:43 +0800

    update

--- fanyi/admin.py
+++ fanyi/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+from fanyi import models
+
+admin.site.register(models.Business)
+admin.site.register(models.Application)
+admin.site.register(models.UserInfo)
+admin.site.register(models.ReqInfo)
+admin.site.register(models.UserToApp)
