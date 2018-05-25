7b4e2a88abde098fad137a859b0d5d42afb18aff
zjjfx88 <zjj7842806@163.com>
2018-05-25 19:26:02 +0800

    fix bug

--- fanyi/views.py
+++ fanyi/views.py
@@ -237,19 +237,18 @@ def start_monitor_ip(request):
 	req_id = request.POST.get('line_id')
 	try:
 		running_pid = models.Host.objects.filter(id=req_id,status=1).values('runningPID')
-		for item in running_pid:
-			if item['runningPID'] != "":
+		if running_pid:
+			for item in running_pid:
 				os.popen('kill -9 %s' % item['runningPID'])
-			else:
-				close_all_id = models.FyMonitor.objects.filter(status=1,h_id=req_id).values('id')
-				for close_id in close_all_id:
-					models.FyMonitor.objects.filter(id=close_id['id'],h_id=req_id).update(status=0)
-				models.FyMonitor.objects.create(create_time=get_now_time(), user=user_id, status=1, h_id=req_id)
-				running_case_id = models.FyMonitor.objects.filter(status=1,h_id=req_id).values('id')
-				for run_id in running_case_id:
-					print(run_id['id'])
-					child = subprocess.Popen(['/usr/local/bin/python3', '/search/odin/daemon/pyonsg/utils/monitor.py', int(run_id['id'])], shell=False)
-					models.Host.objects.filter(id=req_id).update(runningPID=child.pid)
+		close_all_id = models.FyMonitor.objects.filter(status=1, h_id=req_id).values('id')
+		for close_id in close_all_id:
+			models.FyMonitor.objects.filter(id=close_id['id'], h_id=req_id).update(status=0)
+		models.FyMonitor.objects.create(create_time=get_now_time(), user=user_id, status=1, h_id=req_id)
+		running_case_id = models.FyMonitor.objects.filter(status=1, h_id=req_id).values('id')
+		for run_id in running_case_id:
+			child = subprocess.Popen(['/usr/local/bin/python3', '/search/odin/daemon/pyonsg/utils/monitor.py', str(run_id['id'])], shell=False)
+			models.Host.objects.filter(id=req_id).update(runningPID=child.pid)
+
 	except Exception as e:
 		ret['status'] = False
 		ret['error'] = "Error:" + str(e)
