b2b79598726c92a5b0a3e1c76ec364446700e4dd
zjjfx88 <zjj7842806@163.com>
2018-05-29 19:34:13 +0800

    update monitor

--- utils/monitor.py
+++ utils/monitor.py
@@ -7,6 +7,7 @@ import sys
 import time
 import signal
 import os
+import logUtils
 
 database_host="10.134.110.163"
 database_data="sogotest"
@@ -16,24 +17,26 @@ database_pass="Zhangjj@sogou123"
 monitor_id = int(sys.argv[1])
 host_id = sys.argv[2]
 
+logInfo = logUtils.logutil(monitor_id)
+
 # 主方法
 def ssh_command(user, host, password, command):
     ssh_new_key = 'Are you sure you want to continue connecting'
     child = pexpect.spawn('ssh -l %s %s %s' % (user, host, command))
     i = child.expect([pexpect.TIMEOUT, ssh_new_key, 'password: '])
     if i == 0:
-        print('ERROR!')
-        print('SSH could not login. Here is what SSH said:')
-        print(child.before, child.after)
+        logInfo.log_info('ERROR!')
+        logInfo.log_info('SSH could not login. Here is what SSH said:')
+        logInfo.log_info(child.before, child.after)
         return None
     if i == 1:
         child.sendline('yes')
         child.expect('password: ')
         i = child.expect([pexpect.TIMEOUT, 'password: '])
         if i == 0:
-            print('ERROR!')
-            print('SSH could not login. Here is what SSH said:')
-            print(child.before, child.after)
+            logInfo.log_info('ERROR!')
+            logInfo.log_info('SSH could not login. Here is what SSH said:')
+            logInfo.log_info(child.before, child.after)
             return None
     child.sendline(password)
     return child
@@ -54,16 +57,19 @@ def gpu_info():
         db = pymysql.connect(database_host, database_user, database_pass, database_data)
         cursor = db.cursor()
         sql = "UPDATE %s set gpumem=CONCAT(gpumem, '%s'),gpumemused=CONCAT(gpumemused, '%s') where id=%d;" % (database_table, gpumeminfo, gpumemused,monitor_id)
+        logInfo.log_info('gpu mem is '+g_mem+' and gpu mem used is '+g_used)
         cursor.execute(sql)
         try:
             db.commit()
         except:
             db.rollback()
+            logInfo.log_info('Insert a piece of data failed')
         leave_num += 1
         time.sleep(5)
 
 
 def sig_handler(sig, frame):
+    logInfo.log_info('Revice close signal and exit monitor')
     sys.exit()
 
 signal.signal(signal.SIGTERM, sig_handler)
@@ -79,9 +85,11 @@ if __name__ == '__main__':
         cursor.execute(sql)
         try:
             db.commit()
-        except:
+            logInfo.log_info('Insert a new task sql is '+ sql)
+        except Exception as e:
             db.rollback()
+            logInfo.log_info('Insert a new task failed')
         gpu_info()
 
     except Exception as e:
-        pass
+        logInfo.log_info('Start Monitor failed')
